e0560ca56448c0763730e1139f08288fa9ba9dce
Khriz Enríquez <christoferen7@gmail.com>
2014-04-30 00:35:47 -0600

    creación archivo colaArchivoBinario
    Se crea el archivo colaArchivoBinario, por el momento solo las
    instrucciones generales se crearon.

---
+++ colaArchivoBinario.cpp
@@ -0,0 +1,13 @@
+/*
+	@autor: 		Khriz Enríquez
+	@nombreApp:		colaArchivoBinario
+	@descripción:	Manejar una cola que tenga la opción de almancenar los datos en un archivo
+					binario.
+
+	El menú tendrá las opciones:
+		-1. Carga datos: cargará los datos de un archivo binario y los colocará dentro de la estructura de datos
+		-2. Agregar datos: agregará los datos a la cola creada
+		-3. Eliminar dato de cola: eliminará el primer dato de la cola
+		-4. Guardar: guardará los datos de la estructura de datos en el archivo binario
+		-5. Salir
+*/
