d53a18ddf3f4b168e9b2dd8be0ae5775853a97f9
Khriz Enríquez <christoferen7@gmail.com>
2014-06-04 02:12:42 -0600

    Se termino el menú predeterminado

--- proyectoProgra1.cpp
+++ proyectoProgra1.cpp
@@ -1,26 +1,17 @@
 /*
-	@autor: Khriz Enríquez khrizenriquez
+	**********************************************************************************************************
+	**@autor: 			Khriz Enríquez @khrizenriquez
+	**
+	**@Descripción: 	Crear un menú dinámico con las siguientes opciones
+	**						Alta/creación de opciones, Modificación de opciones, Eliminación de opciones
+	**
+	**					Ejecución del menú: Lectura del menú, Mostrar menú, Ejecutar opciones
+	**
+	**Segun los 7 ejercicios que se han hecho se complementarán con este programa, los cuales son:
+	**	Pilas, Colas, Recursividad, Archivos secuenciales, Archivos binarios
+	**********************************************************************************************************
 
-	Crear un menú dinámico
-		-Alta/creación de opciones
-		-Modificación de opciones
-		-Eliminación de opciones
-
-	Ejecución del menú
-		-Lectura del menú
-		-Mostrar menú
-		Ejecutar opciones
-
-	Segun los 7 ejercicios que se han hecho se complementarán con este programa, los cuales son:
-		Pilas
-		Colas
-		Recursividad
-		Archivos secuenciales
-		Archivos binarios
-*/
-
-/*
-	para manejo de archivos en c++
+	Para manejo de archivos en c++
 	r: sólo lectura. El fichero debe existir.
 	w: se abre para escritura, se crea un fichero nuevo o se sobrescribe si ya existe.
 	a: añadir, se abre para escritura, el cursor se situa al final del fichero. Si el fichero no existe, se crea.
@@ -46,14 +37,35 @@
 #include <windows.h>//para usar el sleep
 
 using namespace std;
-
-/*	variables globales*/
+/*********************************************************************************************
+										variables globales
+**********************************************************************************************/
 char nombreUsuario[50];
 int opcionMenuPrimario = 0;
-
-/*	funciones y procedimientos*/
+/*cuando una pila esta vacia se coloca el valor -1 por estandar https://www.youtube.com/watch?v=yE0aMD5IueE*/
+int top = -1;
+/*el bottom tendrá el valor del primer arreglo*/
+int bottom = 0;
+/*	tendrá 10 posiciones la pila, no puedo generar un elemento como un JSon para que reciba varios valores
+entonces lo mas cernano será una pila en forma de arreglo*/
+struct pila{
+	int id;//identificador del elemento de la pila
+	char valor[50];
+} pila1[ 10 ];
+int topBinario = -1;
+//para saber cual es el indice de mi estructura de datos
+int bottomBinario = 0;
+FILE *fArchivo;
+struct datosArchivo{
+	int id;
+	char datos[50];
+} dArchivo[10];
+/**********************************************************************************************	
+									funciones y procedimientos
+**********************************************************************************************/
 void menuDeOpciones();
 void menuDinamico();
+void menuPredeterminado();
 void menuPrincipal() {
 	system("cls");
 	printf("************************************************************\n");
@@ -300,6 +312,320 @@ int fibonacci( int n ) {
 		return 0;
 }
 
+/*	Menú de las pilas*/
+void menuPilas () {
+	//	limpio la pantalla de cualquier cosa que tenga escrita en ella
+	int opcion = 0;
+	printf("*******************************************\n");
+	printf("El programa de pilas esta relacionado con el de colas\n\n");
+	printf("Programa de pilas\nBienvenido super usuario %s que opcion deseas usar:\n\n", nombreUsuario);
+	printf("1.	Push\n");
+	printf("2.	Pop\n");
+	printf("3.	Mostrar la pila actual\n");
+	printf("4.	Regresar al menu anterior\n");
+	printf("*******************************************\n\n");
+
+	cin >> opcion;
+
+	if ( opcion == 1 ) {
+		/* En la opción 1 sera push que se encargará de ingresar los valores a el arreglo */
+		if ( top == -1 ){
+			printf("La pila esta vacia por el momento\n\n");
+			bottom = 0;
+
+			/*comienzo a llenar los datos de la estructura, lleno los datos de uno en uno y luego
+			de haber llenado un dato le pregunto al usuario que desea hacer, 
+
+			Si esta entrando a esta parte es porque no hay valores entonces puedo ingresar valores en 
+			la posición 0 del arreglo */
+			cout << "Ingresa el identificador	: "; cin >> pila1[0].id;
+			cout << "Ingresa el nombre 		: "; cin >> pila1[0].valor;
+			/*en cada pasada de ciclo aumento en 1 el valor de top, esto hace que pueda tener el control de 
+			cuantos valores se han ingresado y llenado*/
+			top++;
+			menuPilas();
+		}else{
+			if ( top > 7 ) {
+				printf("Ya llegaste al limite de los valores que puedes ingresar.\n");
+				Sleep(2000);
+				menuPilas();
+			}else {
+				/*en el momento que ingrese acá, es porque el arreglo ya no esta vacío*/
+				cout << "Ingresa el identificador no. "; cout << ( top + 2 );//
+				cout << "	: "; cin >> pila1[ ( top + 1 ) ].id;//1 mas top para hacer el valor correcto del arreglo
+				cout << "Ingresa el nombre no. "; cout << ( top + 2 );
+				cout << "		: "; cin >> pila1[ ( top + 1 ) ].valor;
+				/*en cada pasada de ciclo aumento en 1 el valor de top, esto hace que pueda tener el control de 
+				cuantos valores se han ingresado y llenado*/
+				top++;
+				menuPilas();
+			}
+		}
+	}else
+	if ( opcion == 2 ) {
+		/* la opcion 2 contrendrá el pop que se encargará de sacar al elemento del arreglo, se esta trabajando
+		como filo ( first in last out ), esto quiere decir que el primero en entrar será el último en salir y el ultimo en entrar deberá
+		ser el que se elimina del arreglo */
+		if ( top == -1 ){
+			printf("La pila en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuPilas();
+		}else{
+			/*si la pila no se encuentra vacía*/
+			printf("\n\n");
+			top--;//para reducir el tamaño de mi limite
+			for (int i = bottom; i <= top; i++ ) {
+				pila1[ i ].id;
+				pila1[ i ].valor;
+			}
+			printf("Se ha hecho pop en la pila actual.\n\n");
+			menuPilas ();
+		}
+	}else
+	if ( opcion == 3 ) {
+		/* En la opción 3 hago que se muestren los valores actuales de la pila
+		en la variable top obtengo el valor de cuantos elementos tengo por ahora en mi arreglo */
+		//lo primero que debo hacer es condicionar y si el top es igual a -1 quiere decir que la pila esta vacía
+		if ( top == -1 ){
+			printf("La pila en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuPilas();
+		}else{
+			system("cls");
+			/*si la pila no se encuentra vacía*/
+			printf("\n\nLos valores actuales de la pila son:\n\n");
+			for (int i = bottom; i <= top; i++ ) {
+				cout << (i + 1); cout << ". "; cout << pila1[ i ].id; cout << " | "; cout << pila1[ i ].valor; cout << endl;
+			}
+			printf("\n\n");
+			menuPilas ();
+		}
+	}else
+	if ( opcion == 4 ) {
+		/* en la opcion 3 saldrá del proceso actual y se cerrará la aplicación */
+		system("cls");//limpio la pantalla
+		menuPredeterminado ();
+	}else {
+		system("cls");
+		menuPilas();
+	}
+}
+
+/*	Menú de colas*/
+void menuColas () {
+	//	limpio la pantalla de cualquier cosa que tenga escrita en ella
+	int opcion = 0;
+	printf("*******************************************\n");
+	printf("El programa de Colas esta relacionado con el de pilas\n\n");
+	printf("Programa de colas\nBienvenido super usuario %s que opcion deseas usar:\n\n", nombreUsuario);
+	printf("1.	Agregar datos a la cola\n");
+	printf("2.	Eliminar dato de la cola\n");
+	printf("3.	Mostrar la cola actual\n");
+	printf("4.	Regresar al menu anterior\n");
+	printf("*******************************************\n\n");
+
+	cin >> opcion;
+
+	if ( opcion == 1 ) {
+		/* En la opción 1 sera push que se encargará de ingresar los valores a el arreglo */
+		if ( top == -1 ){
+			printf("La cola esta vacia por el momento\n\n");
+
+			bottom = 0;
+
+			/*comienzo a llenar los datos de la estructura, lleno los datos de uno en uno y luego
+			de haber llenado un dato le pregunto al usuario que desea hacer, 
+
+			Si esta entrando a esta parte es porque no hay valores entonces puedo ingresar valores en 
+			la posición 0 del arreglo */
+			cout << "Ingresa el identificador	: "; cin >> pila1[0].id;
+			cout << "Ingresa el nombre 		: "; cin >> pila1[0].valor;
+			/*en cada pasada de ciclo aumento en 1 el valor de top, esto hace que pueda tener el control de 
+			cuantos valores se han ingresado y llenado*/
+			top++;
+			menuColas();
+		}else{
+			if ( top > 7 ) {
+				printf("Vacia la cola para poder ingresar valores.\n");
+				Sleep(2000);
+				menuColas();
+			}else {
+				/*en el momento que ingrese acá, es porque el arreglo ya no esta vacío*/
+				cout << "Ingresa el identificador no. "; cout << ( top + 2 );//
+				cout << "	: "; cin >> pila1[ ( top + 1 ) ].id;//1 mas top para hacer el valor correcto del arreglo
+				cout << "Ingresa el nombre no. "; cout << ( top + 2 ); 
+				cout << "		: "; cin >> pila1[ ( top + 1 ) ].valor;
+				/*en cada pasada de ciclo aumento en 1 el valor de top, esto hace que pueda tener el control de 
+				cuantos valores se han ingresado y llenado*/
+				top++;
+				menuColas();
+			}
+		}
+	}else
+	if ( opcion == 2 ) {
+		/*la opcion 2 hará el pop trabajando como fifo ( first in first out )*/
+		if ( top == -1 ){
+			printf("La cola en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuColas();
+		}else{
+			/*si la cola no se encuentra vacía*/
+			printf("\n\n");
+			bottom++;//para aumentar el valor del indice al cual esta apuntando la variable bottom
+			for ( int i = bottom; i <= top; i++ ) {
+				pila1[ i ].id;
+				pila1[ i ].valor;
+			}
+			printf("Se extrajo el dato de la cola.\n\n");
+			menuColas ();
+		}
+	}else
+	if ( opcion == 3 ) {
+		/* En la opción 3 hago que se muestren los valores actuales de la pila
+		en la variable top obtengo el valor de cuantos elementos tengo por ahora en mi arreglo */
+		//lo primero que debo hacer es condicionar y si el top es igual a -1 quiere decir que la pila esta vacía
+		if ( top == -1 ){
+			printf("La cola en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuColas();
+		}else{
+			system("cls");
+			/*si la cola no se encuentra vacía*/
+			printf("\n\nLos valores actuales de la cola son:\n\n");
+			for (int i = bottom; i <= top; i++ ) {
+				cout << (i + 1); cout << ". "; cout << pila1[ i ].id; cout << " | "; cout << pila1[ i ].valor; cout << endl;
+			}
+			printf("\n\n");
+			menuColas ();
+		}
+	}else
+	if ( opcion == 4 ) {
+		/* en la opcion 3 saldrá del proceso actual y se cerrará la aplicación */
+		system("cls");//limpio la pantalla
+		menuPredeterminado();
+	}else {
+		system("cls");
+		menuColas();
+	}
+}
+
+/*	Menú de archivos binarios*/
+void menuArchivosB (){
+	int opciones = 0;//será la variable que llevará el control de la opción que se presiona
+	
+	printf("***********************************************************************\n");
+	printf("Bienvenido mega usuario %s, puedes escoger lo que desees de estas opciones\n", nombreUsuario);
+	printf("1.	Agregar datos\n");
+	printf("2.	Eliminar datos\n");
+	printf("3.	Mostrar los datos actuales\n");
+	printf("4.	Guardar\n");
+	printf("5.	Salir\n");
+	printf("***********************************************************************\n");
+
+	cin >> opciones;
+
+	if ( opciones == 1 ) {
+		/* La opción 1 se encargará de agregar los datos a la cola */
+		if ( topBinario == -1 ){
+			printf("No existen datos en este momento.\n\n");
+
+			/*comienzo a llenar los datos de la estructura, lleno los datos de uno en uno y luego
+			de haber llenado un dato le pregunto al usuario que desea hacer, 
+
+			Si esta entrando a esta parte es porque no hay valores entonces puedo ingresar valores en 
+			la posición 0 del arreglo */
+			cout << "Ingresa el identificador	: "; cin >> dArchivo[0].id;
+			cout << "Ingresa el nombre 		: "; cin >> dArchivo[0].datos;
+			/*en cada pasada de ciclo aumento en 1 el valor de top, esto hace que pueda tener el control de 
+			cuantos valores se han ingresado y llenado*/
+			topBinario++;
+			menuArchivosB();
+		}else{
+			if ( topBinario > 8 ) {
+				printf("Ya llegaste al limite de los valores que puedes ingresar.\n");
+				Sleep(2000);
+				menuArchivosB();
+			}else {
+				/*en el momento que ingrese acá, es porque el arreglo ya no esta vacío*/
+				cout << "Ingresa el identificador no. "; cout << ( topBinario + 2 );//
+				cout << "	: "; cin >> dArchivo[ ( topBinario + 1 ) ].id;//1 mas top para hacer el valor correcto del arreglo
+				cout << "Ingresa el nombre no. "; cout << ( topBinario + 2 ); 
+				cout << "		: "; cin >> dArchivo[ ( topBinario + 1 ) ].datos;
+				/*en cada pasada de ciclo aumento en 1 el valor de top, esto hace que pueda tener el control de 
+				cuantos valores se han ingresado y llenado*/
+				topBinario++;
+				menuArchivosB();
+			}
+		}
+	}else
+	if ( opciones == 2 ) {
+		/*la opción 2 se encargará de eliminar los datos de la cola actual siguiendo el principio fifo( first in first out )*/
+		if ( topBinario == -1 ){
+			printf("La cola en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuArchivosB();
+		}else{
+			/*si la cola no se encuentra vacía*/
+			printf("\n\n");
+			bottom++;//para aumentar el valor del indice al cual esta apuntando la variable bottom
+			for (int i = bottom; i <= topBinario; i++ ) {
+				dArchivo[ i ].id;
+				dArchivo[ i ].datos;
+			}
+			printf("Se extrajo el dato de la cola.\n\n");
+			menuArchivosB ();
+		}
+	}else
+	if ( opciones == 3 ) {
+		/* La opción 3 mostrará los datos actuales que se encuentren dentro de la cola */
+		if ( topBinario == -1 ){
+			printf("La cola en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuArchivosB();
+		}else{
+			/*si la cola no se encuentra vacía*/
+			printf("\n\n");
+			for (int i = bottom; i <= topBinario; i++ ) {
+				cout << (i + 1); cout << ". "; cout << dArchivo[ i ].id; cout << " | "; cout << dArchivo[ i ].datos; cout << endl;
+			}
+			printf("\n\n");
+			menuArchivosB ();
+		}
+	}else
+	if ( opciones == 4 ) {
+		/* La opción 4 guardará los datos que se encuentren en la estructura de datos a el archivo binario */
+		if ( topBinario == -1 ){
+			printf("La cola en este momento se encuentra vacia, en 5 segundos \nregresaras al menu principal\n\n");
+			Sleep(5000);
+			menuArchivosB();
+		}else{
+			/*si la cola no se encuentra vacía*/
+			if ( ( fArchivo = fopen( "colasArchivoBinario.dat", "ab+" ) ) == NULL ) {
+				/* Si entra acá es xq ocurrio un error inesperado */
+				printf("Te estan troleando y no quieren que abras la aplicacion.\n");
+			}else{
+				/*primero obtengo la información del archivo, luego eso se lo cargo a mi estructura de datos.
+				La limitante que tengo es que mi estructura tiene una una capacidad maxima de 10 (es un arreglo de 10 posiciones)*/
+				printf("\n\n");
+				for (int i = bottom; i <= topBinario; i++ ) {
+					fprintf( fArchivo, "%i %s\n", dArchivo[i].id, dArchivo[i].datos );
+				}
+				fclose( fArchivo );
+				printf("Datos guardados en el archivo.\n\n");
+			}
+		}
+		menuArchivosB ();
+	}else
+	if ( opciones == 5 ) {
+		/* Saldrá de la aplicación */
+		system("cls");
+		menuPredeterminado();
+	}else{
+		/*Si la opción que presiona no esta dentro del menu lo mando al mismo menú de nuevo*/
+		menuArchivosB ();
+	}
+}
+
 void menuPredeterminado(){
 	int opcionMenuP;
 
@@ -316,14 +642,16 @@ void menuPredeterminado(){
 	cin >> opcionMenuP;
 
 	if ( opcionMenuP == 1 ){
+		menuPilas ();
 	}else
 	if ( opcionMenuP == 2 ){
+		menuColas ();
 	}else
 	if ( opcionMenuP == 3 ){
 		system( "cls" );
 
 		/*	fuente: http://programavideojuegos.blogspot.com/2013/05/sucesion-de-fibonacci-en-c.html*/
-		printf("%s para este ejemplo de recursividad se trabajara con la serie fibonacci\n\n");
+		printf("Hey %s para este ejemplo de recursividad se trabajara con la serie fibonacci\n\n");
 		printf("La sucesion de Fibonacci es una sucesion infinita de numeros en los que cada uno de ellos es el resultado de la suma de sus dos inmediatamente anteriores. La sucesion comienza tal que asi: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987..., estos son los numeros iterando hasta 20 veces:\n\n\n", nombreUsuario);
 		Sleep( 2000 );
 		int num;
@@ -338,15 +666,12 @@ void menuPredeterminado(){
 
 		printf("Hey %s, te cuento que para el ejemplo de archivos secuenciales se creara o sobre escribira un archivo con los datos de un texto Lorem para efecto de este ejemplo, luego de crearce el archivo se guardaran de un texto Lorem en el y de manera imediata se leeran y mostraran en pantalla.\n", nombreUsuario );
 		FILE *fArchivoSecuencial;
-		if ( ( fArchivoSecuencial =  fopen( "fArchivoSecuencial.dat", "wb+" ) ) == NULL ) {
+		if ( ( fArchivoSecuencial =  fopen( "fArchivoSecuencial.txt", "wt+" ) ) == NULL ) {
 			printf("Algo extraño esta pasando, puede que te esten troleando :S\nO la radiacion no permite que uses esta aplicacion :(");
 			printf("\nPor esa razon no podras guardar opciones al menu dinamico\n\n");
 			Sleep( 5000 );
 			menuPredeterminado();
 		}else{
-			/*for ( int i = 0; i <= 50; i++ ) {
-				fprintf( fArchivoSecuencial, "%i \n", i );
-			}*/
 			fprintf( fArchivoSecuencial, "%s \n", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, recusandae nam magnam quam nihil eius soluta quae expedita illo accusamus commodi explicabo sapiente sit deleniti labore fugiat natus inventore laboriosam." );
 			printf("\n\n");
 			fclose( fArchivoSecuencial );
@@ -354,14 +679,13 @@ void menuPredeterminado(){
 		}
 		printf("Los datos guardados son: \n");
 
-		if ( ( fArchivoSecuencial = fopen( "fArchivoSecuencial.dat", "rb+" ) ) == NULL ) {
+		if ( ( fArchivoSecuencial = fopen( "fArchivoSecuencial.txt", "rt+" ) ) == NULL ) {
 			printf("Algo extraño esta pasando, puede que te esten troleando :S\nO la radiacion no permite que uses esta aplicacion :(");
 			printf("\nPor esa razon no podras guardar opciones al menu dinamico\n\n");
 			Sleep( 5000 );
 			menuPredeterminado();
 		}else{
 			char datosArchivoSec[50];
-			printf("Chuta\n");
 			while( !feof( fArchivoSecuencial ) ) {
 				fscanf( fArchivoSecuencial, "%s \n", datosArchivoSec );
 				printf("%s ", datosArchivoSec );
@@ -373,9 +697,10 @@ void menuPredeterminado(){
 		menuPredeterminado();
 	}else
 	if ( opcionMenuP == 5 ){
+		menuArchivosB();
 	}else
 	if ( opcionMenuP == 6 ){
-		menuPredeterminado();
+		menuDeOpciones();
 	}else{
 		menuPredeterminado();
 	}
